81065658c0b6f7984ae6bdbe7994a0bcd99c5ae6
ur2ku <lim8667@gmail.com>
2022-05-06 11:07:10 +0900

    [] useEffect에 조건 주는 이유: 불필요한 자원 절약 성능 측면에서 도움이 된다.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React, {useEffect, useState} from "react";
+import {Text, View} from "react-native";
 
 const App = () => {
     const [count, setCount] = useState(1);
+    const [mcount, setMCount] = useState(1);
     const [name, setName] = useState();
-    const handleCount = () => {
-        setCount(count + 1)
-    }
-    const handleInputChange = (e) => {
-        setName(e.target.value);
-    }
+    const handleCount = () => {setCount(count + 1)}
+    const handleInputChange = (e) => {setName(e.target.value);}
+    const handleMultiplication = (e) => {setMCount(mcount * 2)}
+
+    // 최초 렌더링 될때 실행
     useEffect(() => {
-        console.log(count);
-        console.log(`렌더링 될때마다 실행`)
-    })
-    useEffect(() => {
-        console.log(count);
-        console.log(`처음 렌더링 될때만 실행`);
+        console.log(`최초 렌더링될때 1회`)
     }, [])
+
+    // 메인 렌더링 될때마다 실행
+    useEffect(() => {
+        console.log(`메인 렌더링`)
+    })
+
+    // 마운팅 + count가 변화할때마다 실행
+    useEffect(() => {
+        console.log(count);
+        console.log(`count 변화`);
+    }, [count])
+
+    // 마운팅 + name이 변화할 때마다 실행
+    useEffect(()=>{
+        console.log(`name 변화`);
+    }, [name])
+
+    useEffect(()=>{
+        console.log(`mcount 변화`);
+    }, [mcount])
+
     return (
-        <div>
+        <View>
             <button onClick={handleCount}>Update</button>
-            <span>count: {count}</span>
+            <Text>count: {count} </Text>
             <input type="text" onChange={handleInputChange}/>
-            <span>name: {name}</span>
-        </div>
+            <Text>name: {name}</Text>
+            <br/>
+            <button onClick={handleMultiplication}>Multiplication</button>
+            <Text> mcount: {mcount}</Text>
+        </View>
     )
 }
 
