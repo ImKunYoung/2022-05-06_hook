d4069bfaed151a1cf35be01056886cf14276a2c1
ur2ku <lim8667@gmail.com>
2022-05-06 10:30:03 +0900

    [] textinput에 값이 들어올 때 마다 렌더링

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, {useEffect, useState} from "react";
 
 const App = () => {
     const [count, setCount] = useState(1);
+    const [name, setName] = useState();
     const handleCount = () => {
         setCount(count + 1)
-    };
-
-    useEffect(()=>{
-        console.log(`eeee`)
+    }
+    const handleInputChange = (e) => {
+        setName(e.target.value);
+    }
+    useEffect(() => {
+        console.log(count);
+        console.log(`렌더링 될때마다 실행`)
     })
-    useEffect(()=>{
-        console.log(`eeee`)
-    },[])
+    useEffect(() => {
+        console.log(count);
+        console.log(`처음 렌더링 될때만 실행`);
+    }, [])
     return (
         <div>
-        <button onClick={handleCount}>Update</button>
-        <span>count: {count}</span>
+            <button onClick={handleCount}>Update</button>
+            <span>count: {count}</span>
+            <input type="text" onChange={handleInputChange}/>
+            <span>name: {name}</span>
         </div>
-    );
+    )
 }
 
 export default App;
