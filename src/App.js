e225602221e54fc9fccf8d3fd1185d03c19f96f9
ur2ku <lim8667@gmail.com>
2022-05-06 09:57:17 +0900

    [init]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const App = () => {
-    return(
-        <>
-        </>
+    const [count, setCount] = useState(1);
+    const handleCount = () => {
+        setCount(count + 1)
+    };
+
+   
+    return (
+        <div>
+        <button onClick={handleCount}>Update</button>
+        <span>count: {count}</span>
+        </div>
     );
 }
 
